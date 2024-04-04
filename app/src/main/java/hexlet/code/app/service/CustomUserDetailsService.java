8b320c8f629dce506fb9f746402041963f55a2b2
serebrianskii <serebrianskiik@gmail.com>
2024-04-04 13:01:41 +0200

    Change creation of first pre-made user

--- app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
+++ app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.service;
 
+import hexlet.code.app.dto.user.UserCreateDTO;
 import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,8 +10,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
-
-import hexlet.code.app.model.User;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsManager {
@@ -20,21 +20,24 @@ public class CustomUserDetailsService implements UserDetailsManager {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Override
-    public void createUser(UserDetails userData) {
-        var user = new User();
-        user.setEmail(userData.getUsername());
-        var hashedPassword = passwordEncoder.encode(userData.getPassword());
-        user.setEncryptedPassword(hashedPassword);
-        userRepository.save(user);
-
-    }
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     // We use unique email as username
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public void createUser(UserDetails userData) {
+        var userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setEmail(userData.getUsername());
+        var encryptedPassword = passwordEncoder.encode(userData.getPassword());
+        userCreateDTO.setPassword(encryptedPassword);
+        var user = userMapper.map(userCreateDTO);
+        userRepository.save(user);
     }
 
     @Override
