d1a3337c15a20a25caf33f5456ef638a8d770030
serebrianskii <serebrianskiik@gmail.com>
2024-04-08 16:40:06 +0200

    Implement initialization of pre-made task statuses

--- app/src/main/java/hexlet/code/app/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -1,6 +1,9 @@
 package hexlet.code.app.component;
 
+import hexlet.code.app.dto.task_status.TaskStatusCreateDTO;
+import hexlet.code.app.mapper.TaskStatusMapper;
 import hexlet.code.app.model.User;
+import hexlet.code.app.repository.TaskStatusRepository;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
@@ -8,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @AllArgsConstructor
@@ -18,6 +23,12 @@ public class DataInitializer implements ApplicationRunner {
 
     @Autowired
     private final CustomUserDetailsService userService;
+
+    @Autowired
+    private final TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private final TaskStatusMapper taskStatusMapper;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -31,6 +42,23 @@ public class DataInitializer implements ApplicationRunner {
             userService.createUser(userData);
         }
 
+        var defaultSlugs = Map.of(
+                "draft", "Draft",
+                "to_review", "To review",
+                "to_be_fixed", "To be fixed",
+                "to_publish", "To publish",
+                "published", "Published"
+        );
+        for (var entry : defaultSlugs.entrySet()) {
+            var taskStatusCreateDTO = new TaskStatusCreateDTO();
+            taskStatusCreateDTO.setSlug(entry.getKey());
+            taskStatusCreateDTO.setName(entry.getValue());
+            var taskStatus = taskStatusMapper.map(taskStatusCreateDTO);
+            taskStatusRepository.save(taskStatus);
+        }
+
+
+        // TODO separate methods?
         // TODO create faker users with tasks and assignments
 
     }
