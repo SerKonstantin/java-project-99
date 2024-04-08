67a42b183e35e1694bd657b04b66197ecef32cd4
serebrianskii <serebrianskiik@gmail.com>
2024-04-08 16:17:18 +0200

    Fix bugs in tests for UserController

--- app/src/test/java/hexlet/code/app/controller/api/UserControllerTest.java
+++ app/src/test/java/hexlet/code/app/controller/api/UserControllerTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.app.dto.user.UserCreateDTO;
+import hexlet.code.app.dto.user.UserUpdateDTO;
 import hexlet.code.app.exception.ResourceNotFoundException;
 import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.model.User;
@@ -21,8 +22,11 @@ import org.instancio.Select;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,10 +35,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 
-import java.util.Map;
+import java.util.stream.Stream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // Annotation required to use Faker in parametrized tests
 public class UserControllerTest {
 
     @Autowired
@@ -61,7 +66,6 @@ public class UserControllerTest {
 
     @BeforeEach
     public void setUp() {
-        token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
         var hashedPassword = passwordEncoder.encode(faker.internet().password(3, 20));
         var userData = Instancio.of(UserCreateDTO.class)
                 .supply(Select.field(UserCreateDTO::getFirstName), () -> faker.name().firstName())
@@ -72,6 +76,7 @@ public class UserControllerTest {
         testUser = userMapper.map(userData);
         userRepository.save(testUser);
 
+        token = jwt().jwt(builder -> builder.subject(testUser.getEmail()));
     }
 
     @AfterEach
@@ -92,12 +97,11 @@ public class UserControllerTest {
 
     @Test
     public void testCreate() throws Exception {
-        var createData = Map.of(
-                "firstName", faker.name().firstName(),
-                "lastName", faker.name().lastName(),
-                "email", faker.internet().emailAddress(),
-                "password", faker.internet().password(3, 20)
-        );
+        var createData = new UserCreateDTO();
+        createData.setFirstName(faker.name().firstName());
+        createData.setLastName(faker.name().lastName());
+        createData.setEmail(faker.internet().emailAddress());
+        createData.setPassword(faker.internet().password(3, 20));
 
         var request = post("/api/users")
                 .with(token)
@@ -106,20 +110,19 @@ public class UserControllerTest {
 
         mockMvc.perform(request).andExpect(status().isCreated());
 
-        var user = userRepository.findByEmail(createData.get("email"))
+        var user = userRepository.findByEmail(createData.getEmail())
                 .orElseThrow(() -> new ResourceNotFoundException("\ntestCreate() in UserControllerTest failed\n"));
 
-        assertThat(user.getFirstName()).isEqualTo(createData.get("firstName"));
-        assertThat(user.getLastName()).isEqualTo(createData.get("lastName"));
-        assertThat(user.getEncryptedPassword()).isNotEqualTo(createData.get("password"));
+        assertThat(user.getFirstName()).isEqualTo(createData.getFirstName());
+        assertThat(user.getLastName()).isEqualTo(createData.getLastName());
+        assertThat(user.getEncryptedPassword()).isNotEqualTo(createData.getPassword());
     }
 
     @Test
     public void testCreateWithoutNames() throws Exception {
-        var createData = Map.of(
-                "email", faker.internet().emailAddress(),
-                "password", faker.internet().password(3, 20)
-        );
+        var createData = new UserCreateDTO();
+        createData.setEmail(faker.internet().emailAddress());
+        createData.setPassword(faker.internet().password(3, 20));
 
         var request = post("/api/users")
                 .with(token)
@@ -128,33 +131,20 @@ public class UserControllerTest {
 
         mockMvc.perform(request).andExpect(status().isCreated());
 
-        var user = userRepository.findByEmail(createData.get("email"))
+        var user = userRepository.findByEmail(createData.getEmail())
                 .orElseThrow(() -> new ResourceNotFoundException("\ntestCreate() in UserControllerTest failed\n"));
 
         assertThat(user.getFirstName()).isNull();
         assertThat(user.getLastName()).isNull();
-        assertThat(user.getEncryptedPassword()).isNotEqualTo(createData.get("password"));
+        assertThat(user.getEncryptedPassword()).isNotEqualTo(createData.getPassword());
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "faker.internet().emailAddress(), 'ab'",
-            "faker.internet().emailAddress(), ''",
-            "faker.internet().emailAddress(), null",
-            "faker.internet().emailAddress(), Optional.empty()",
-            "'not_email', faker.internet().password(3, 20)",
-            "'', faker.internet().password(3, 20)",
-            "null, faker.internet().password(3, 20)",
-            "Optional.empty(), faker.internet().password(3, 20)"
-
-    })
+    @MethodSource("supplyWithCreateAndUpdateInvalidData")
     public void testCreateWithInvalidData(String email, String password) throws Exception {
-        var createData = Map.of(
-                "firstName", faker.name().firstName(),
-                "lastName", faker.name().lastName(),
-                "email", email,
-                "password", password
-        );
+        var createData = new UserCreateDTO();
+        createData.setEmail(email);
+        createData.setPassword(password);
 
         var request = post("/api/users")
                 .with(token)
@@ -162,6 +152,17 @@ public class UserControllerTest {
                 .content(om.writeValueAsString(createData));
 
         mockMvc.perform(request).andExpect(status().isBadRequest());
+    }
+
+    private Stream<Arguments> supplyWithCreateAndUpdateInvalidData() {
+        return Stream.of(
+                Arguments.of(faker.internet().emailAddress(), "pw"),
+                Arguments.of(faker.internet().emailAddress(), ""),
+                Arguments.of(faker.internet().emailAddress(), null),
+                Arguments.of("not_email", faker.internet().password(3, 20)),
+                Arguments.of("", faker.internet().password(3, 20)),
+                Arguments.of(null, faker.internet().password(3, 20))
+        );
     }
 
     @Test
@@ -200,12 +201,11 @@ public class UserControllerTest {
     public void testUpdate() throws Exception {
         var id = testUser.getId();
 
-        var updateData = Map.of(
-                "firstName", faker.name().firstName(),
-                "lastName", faker.name().lastName(),
-                "email", faker.internet().emailAddress(),
-                "password", faker.internet().password(3, 20)
-        );
+        var updateData = new UserUpdateDTO();
+        updateData.setFirstName(JsonNullable.of(faker.name().firstName()));
+        updateData.setLastName(JsonNullable.of(faker.name().lastName()));
+        updateData.setEmail(JsonNullable.of(faker.internet().emailAddress()));
+        updateData.setPassword(JsonNullable.of(faker.internet().password(3, 20)));
 
         var request = put("/api/users/{id}", id)
                 .with(token)
@@ -217,19 +217,18 @@ public class UserControllerTest {
         var updatedUser = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("\ntestUpdate in UserControllerTest failed\n"));
 
-        assertThat(updatedUser.getEmail()).isEqualTo(updateData.get("email"));
-        assertThat(updatedUser.getFirstName()).isEqualTo(updateData.get("firstName"));
-        assertThat(updatedUser.getLastName()).isEqualTo(updateData.get("lastName"));
-        assertThat(updatedUser.getEncryptedPassword()).isNotEqualTo(updateData.get("password"));
+        assertThat(updatedUser.getEmail()).isEqualTo(updateData.getEmail().get());
+        assertThat(updatedUser.getFirstName()).isEqualTo(updateData.getFirstName().get());
+        assertThat(updatedUser.getLastName()).isEqualTo(updateData.getLastName().get());
+        assertThat(updatedUser.getEncryptedPassword()).isNotEqualTo(updateData.getPassword().get());
     }
 
     @Test
     public void testPartialUpdate() throws Exception {
         var id = testUser.getId();
 
-        var updateData = Map.of(
-                "firstName", faker.name().firstName()
-        );
+        var updateData = new UserUpdateDTO();
+        updateData.setFirstName(JsonNullable.of(faker.name().firstName()));
 
         var request = put("/api/users/{id}", id)
                 .with(token)
@@ -242,28 +241,17 @@ public class UserControllerTest {
                 .orElseThrow(() -> new ResourceNotFoundException("\ntestUpdate in UserControllerTest failed\n"));
 
         assertThat(updatedUser.getEmail()).isEqualTo(testUser.getEmail());
-        assertThat(updatedUser.getFirstName()).isEqualTo(updateData.get("firstName"));
+        assertThat(updatedUser.getFirstName()).isEqualTo(updateData.getFirstName().get());
         assertThat(updatedUser.getLastName()).isEqualTo(testUser.getLastName());
         assertThat(updatedUser.getEncryptedPassword()).isEqualTo(testUser.getPassword());
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "faker.internet().emailAddress(), 'ab'",
-            "faker.internet().emailAddress(), ''",
-            "faker.internet().emailAddress(), null",
-            "faker.internet().emailAddress(), Optional.empty()",
-            "'not_email', faker.internet().password(3, 20)",
-            "'', faker.internet().password(3, 20)",
-            "null, faker.internet().password(3, 20)",
-            "Optional.empty(), faker.internet().password(3, 20)"
-
-    })
+    @MethodSource("supplyWithCreateAndUpdateInvalidData")
     public void testUpdateWithInvalidData(String email, String password) throws Exception {
-        var updateData = Map.of(
-                "email", email,
-                "password", password
-        );
+        var updateData = new UserUpdateDTO();
+        updateData.setEmail(JsonNullable.of(email));
+        updateData.setPassword(JsonNullable.of(password));
 
         var request = put("/api/users/{id}", testUser.getId())
                 .with(token)
