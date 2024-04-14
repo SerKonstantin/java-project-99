07cfee42f18d8615fce9033f453706b62dd71ed6
serebrianskii <serebrianskiik@gmail.com>
2024-04-14 14:27:55 +0200

    Implement tests for labels controller

--- app/src/test/java/hexlet/code/app/util/ModelUtils.java
+++ app/src/test/java/hexlet/code/app/util/ModelUtils.java
@@ -1,11 +1,14 @@
 package hexlet.code.app.util;
 
+import hexlet.code.app.dto.label.LabelCreateDTO;
 import hexlet.code.app.dto.task.TaskCreateDTO;
 import hexlet.code.app.dto.task_status.TaskStatusCreateDTO;
 import hexlet.code.app.dto.user.UserCreateDTO;
+import hexlet.code.app.mapper.LabelMapper;
 import hexlet.code.app.mapper.TaskMapper;
 import hexlet.code.app.mapper.TaskStatusMapper;
 import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.model.Label;
 import hexlet.code.app.model.Task;
 import hexlet.code.app.model.TaskStatus;
 import hexlet.code.app.model.User;
@@ -16,6 +19,8 @@ import org.instancio.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
 
 @Getter
 @Component
@@ -36,9 +41,13 @@ public class ModelUtils {
     @Autowired
     private TaskMapper taskMapper;
 
+    @Autowired
+    private LabelMapper labelMapper;
+
     private User user;
     private TaskStatus taskStatus;
     private Task task;
+    private Label label;
 
     public ModelUtils generateData() {
         var hashedPassword = passwordEncoder.encode(faker.internet().password(3, 20));
@@ -56,13 +65,20 @@ public class ModelUtils {
                 .create();
         taskStatus = taskStatusMapper.map(taskStatusData);
 
+        var labelData = Instancio.of(LabelCreateDTO.class)
+                .supply(Select.field(LabelCreateDTO::getName), () -> faker.lorem().characters(3, 1000))
+                .create();
+        label = labelMapper.map(labelData);
+
         var taskData = Instancio.of(TaskCreateDTO.class)
                 .supply(Select.field(TaskCreateDTO::getIndex), () -> faker.number().numberBetween(1L, 10000L))
                 .supply(Select.field(TaskCreateDTO::getAssigneeId), () -> user.getId())
                 .supply(Select.field(TaskCreateDTO::getTitle), () -> faker.lorem().word())
                 .supply(Select.field(TaskCreateDTO::getContent), () -> faker.lorem().paragraph())
                 .supply(Select.field(TaskCreateDTO::getStatus), () -> taskStatus.getSlug())
+                .supply(Select.field(TaskCreateDTO::getLabelIds), () -> new HashSet<Long>())
                 .create();
+        taskData.getLabelIds().add(label.getId());
         task = taskMapper.map(taskData);
 
         return this;
