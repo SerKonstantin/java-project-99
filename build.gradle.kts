143b2247990287bc078d6da987f1a349f10ca108
serebrianskii <serebrianskiik@gmail.com>
2024-04-22 18:18:15 +0200

    Fifth attempt to add sentry error tracking

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.8.0")
 
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
