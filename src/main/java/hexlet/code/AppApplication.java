faa3ab11aca97c417e83b1e2afb9bb3af15676f9
serebrianskii <serebrianskiik@gmail.com>
2024-05-02 13:17:26 +0200

    Refactor to move faker bean to a separate config file

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,9 +1,7 @@
 package hexlet.code;
 
-import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,11 +10,6 @@ public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-    }
-
-    @Bean
-    public Faker getFaker() {
-        return new Faker();
     }
 
 }
